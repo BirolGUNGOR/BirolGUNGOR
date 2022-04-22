f7121bb3007799d25ee4a3bafd5912abea734f00
BirolGUNGOR <98694086+BirolGUNGOR@users.noreply.github.com>
2022-04-22 14:57:49 +0300

    Add files via upload

---
+++ harita.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+unesco = pd.read_csv("unesco.csv")
+
+ulkelist = []
+ulkeler = unesco["Country name"].unique()
+for i in ulkeler:
+    ilist = i.split(",")
+    for x in ilist:
+        if x not in ulkelist:
+          ulkelist.append(x)
+
+ulkelist.insert(0,"Tüm Ülkeler")
+
+ulkesec=st.sidebar.selectbox("Ülke seçiniz",ulkelist)
+
+mirassec=st.sidebar.text_input("Miras ismi giriniz")
+
+ulkedf = unesco[unesco["Country name"].str.contains(ulkesec)]
+
+mirasdf=unesco[unesco["Name"].str.contains(mirassec)]
+
+konumlar = ulkedf[["longitude","latitude"]]
+
+if len(mirassec)>3:
+     st.map(mirasdf[["longitude", "latitude"]])
+     df=mirasdf
+elif ulkesec == "Tüm Ülkeler":
+     st.map(unesco[["longitude", "latitude"]])
+     df = unesco
+else:
+     st.map(konumlar)
+     df = ulkedf
+
+# streamlit reference sayfasından download'a baktık
+csv=df.to_csv().encode('utf-8')
+
+st.sidebar.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='Data_Frame.csv',
+     mime='text/csv',
+ )
+
+st.dataframe(df)
+
+# share.streamlit ile url linki olarak paylaşabiliriz
